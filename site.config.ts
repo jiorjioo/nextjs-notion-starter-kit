3aabf298ad66137359f11c49761841d2f9ec9253
Balma Bahira Adzkia <73548075+jiorjioo@users.noreply.github.com>
2023-07-02 11:36:03 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'brainrefujiz',
   github: 'jiorjioo',
-  linkedin: 'balmabahiraa'
+  linkedin: 'balmabahiraa',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
